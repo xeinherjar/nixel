1f0a99d7c7a7ebbaec40d0e182c5fac5bd4c43ab
Alex Pate <proto.merkabah@gmail.com>
2015-03-11 17:33:47 -0400

    start of drag/drop

---
+++ app/scripts/dragdrop.directive.js
@@ -0,0 +1,76 @@
+;(function() {
+  'use strict';
+
+  angular.module('nixel')
+
+  .directive('nixelDrag',
+    function() {
+
+    return {
+      restrict: 'A',
+
+    link: function(scope, el, attr) {
+      el.attr('draggable', 'true');
+      var idx = scope.$index;
+
+      el.bind('dragstart', function(e) {
+        e.dataTransfer.setData('text', idx);
+      });
+    }
+
+
+    };
+    })
+
+  .directive('nixelDrop',
+      ['$rootScope',
+    function($rootScope) {
+
+    return {
+
+      restrict: 'A',
+
+    link: function(scope, el, attr) {
+      el.bind('dragover', function(e) {
+        // Allows us to drop
+        if (e.preventDefault) {
+          e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+
+        return false;
+      });
+
+
+      // Cosmetic
+      el.bind('dragenter', function(e) {
+        angular.element(e.target).addClass('drop-target');
+      });
+
+      el.bind('dragleave', function(e) {
+        angular.element(e.target).removeClass('drop-target');
+      });
+
+      el.bind('drop', function(e) {
+        if (e.preventDefault) {
+          e.preventDefault();
+        }
+        if (e.stopPropogation) {
+          e.stopPropogation();
+        }
+
+        var idx = e.dataTransfer.getData('text');
+        console.log(idx);
+        $rootScope.$broadcast('sprite:drop', idx);
+      });
+
+
+    },
+
+
+    };
+    }
+  ]);
+
+}());
