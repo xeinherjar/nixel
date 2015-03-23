f772fd8b051a9df6c6286637a2de1f7c5ce18506
Alex Pate <proto.merkabah@gmail.com>
2015-03-23 16:30:18 -0400

    build

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -233,6 +233,7 @@
            ['$scope', 'chrFactory', 'romFactory', '$rootScope',
     function($scope,   chrFactory,   romFactory,   $rootScope) {
 
+    $scope.chrAvailable = true;
 
     // from file.controller
     $scope.$on('file:load', function(e) {
@@ -240,16 +241,27 @@
         chrFactory.setup(romFactory.ROM.chrData);
       } else {
         chrFactory.setup(romFactory.ROM.prgData);
+        $scope.chrAvailable = false;
+        $scope.$digest();
       }
 
     });
 
-    // from chr.factory 
+    // from chr.factory
     $scope.$on('chr:chrTable', function(e, chrTable) {
       $scope.spriteTable = chrTable;
       // Needed to fire ng-repeat after ROM load
       $scope.$digest();
     });
+
+
+    $scope.makeHex = function(n) {
+      var h = n.toString(16).toUpperCase();
+      var l = "0000".substring(h.length);
+
+      return "0x" + l + h;
+    };
+
 
 
     }
